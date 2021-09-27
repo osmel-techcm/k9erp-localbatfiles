0e82e4cfea137e9cec58fde109e01f65dc7361b7
Osmel Crespo <osmel.e.crespo@hotmail.com>
2021-09-27 12:17:25 -0400

    Add log app api

--- runFiles.bat
+++ runFiles.bat
@@ -2,6 +2,7 @@ start "Master" /d "..\k9erp-masterapi\masterApi" dotnet run
 start "Tenant" /d "..\k9erp-tenantapi\tenantApi" dotnet run
 start "FM" /d "..\k9erp-filemanagerapi\fileManagerApi" dotnet run
 start "Report" /d "..\k9erp-reportapi\reportApi" dotnet run
+start "Log" /d "..\k9erp-logapi\logApi" dotnet run
 
 start "MasterUi" /d "..\k9erp-masterui" ng serve -o --port 4201
 start "MasterUi" /d "..\k9erp-tenantui" ng serve -o --port 4202
