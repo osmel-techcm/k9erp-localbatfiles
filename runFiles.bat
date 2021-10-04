49a079a14fdd6a0b9340391b78919f170b0e1791
Osmel Crespo <osmel@techcm.com>
2021-10-04 11:15:24 -0400

    Update runFiles.bat

--- runFiles.bat
+++ runFiles.bat
@@ -5,5 +5,5 @@ start "Report" /d "..\k9erp-reportapi\reportApi" dotnet run
 start "Log" /d "..\k9erp-logapi\logApi" dotnet run
 start "Socket" /d "..\k9erp-socket\signalrApi" dotnet run
 
-start "MasterUi" /d "..\k9erp-masterui" ng serve -o --port 4201
-start "MasterUi" /d "..\k9erp-tenantui" ng serve -o --port 4202
+@REM start "MasterUi" /d "..\k9erp-masterui" ng serve -o --port 4201
+@REM start "MasterUi" /d "..\k9erp-tenantui" ng serve -o --port 4202
